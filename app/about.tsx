c3402bee6751287069cac0dc910ba32c52ffee03
Cole <bleeper_genius0c@icloud.com>
2024-01-11 19:52:40 -0600

    About Page

--- app/about.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-
-export default function About() {
-  return <div>About Page</div>
-}
