f782403cec7023ddbc2cd4e1f70d846b30719c66
Cole <bleeper_genius0c@icloud.com>
2024-01-21 02:59:22 -0600

    Homepage + coloring

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="mx-auto flex max-w-[980px] flex-col items-start gap-2">
         <div className="mb-4 items-center justify-center self-center text-center">
           <h1 className="self-center text-5xl font-semibold leading-tight">
-            THE <strong className="font-bold">WIL SANTI</strong> DIFFERENCE
+            THE <strong className="font-bold text-primary">WIL SANTI</strong> DIFFERENCE
           </h1>
           <h3 className="self-center text-2xl font-bold leading-tight">
             B.S., CSCS, CSPS, ACSM E-PHY, USAW 2, USATF I, USAPL SC, ISSN
@@ -16,56 +16,69 @@ export default function About() {
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.bs className="h-24 w-auto flex-none object-contain" />
             <p className="flex-1 text-center sm:text-right">
-              Bachelor of Science Degree in Exercise Science from the University
-              of Rhode Island.
+              <em className="text-primary">
+                Bachelor of Science Degree in Exercise Science
+              </em>{" "}
+              from the University of Rhode Island.
             </p>
           </span>
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.cscs className="order-1 h-24 w-auto flex-none object-contain" />
             <p className="order-2 flex-1 text-center sm:order-none sm:text-left">
-              Certified Strength and Conditioning Specialist (CSCS) through the
-              National Strength and Conditioning Association (NSCA). Certified
-              to apply scientific knowledge to help improve athletic performance
-              for athletes and health/fitness levels for those wanting to reach
-              the peak of living a long, healthy life.
+              <em className="text-primary">
+                Certified Strength and Conditioning Specialist (CSCS)
+              </em>{" "}
+              through the National Strength and Conditioning Association (NSCA).
+              Certified to apply scientific knowledge to help improve athletic
+              performance for athletes and health/fitness levels for those
+              wanting to reach the peak of living a long, healthy life.
             </p>
           </span>
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.csps className="h-24 w-24" />
             <p className="flex-1 text-center sm:text-right">
-              Certified for Special Populations Specialist through the National
-              Strength and Conditioning Association (NSCA). Certified to assess,
-              motivate, educate, and train special population clients of all
-              ages regarding their health and fitness needs, those of whom have
-              chronic and temporary health conditions (ex. heart disease, joint
-              replacement, diabetes).
+              <em className="text-primary">
+                Certified for Special Populations Specialist
+              </em>{" "}
+              through the National Strength and Conditioning Association (NSCA).
+              Certified to assess, motivate, educate, and train special
+              population clients of all ages regarding their health and fitness
+              needs, those of whom have chronic and temporary health conditions
+              (ex. heart disease, joint replacement, diabetes).
             </p>
           </span>
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.acsm className="order-1 h-24 w-auto flex-none object-contain" />
             <p className="order-1 flex-1 text-center sm:order-none sm:text-left">
-              Certified Exercise Physiologist through the American College of
-              Sports Medicine (ACSM). Represents the gold standard by creating
-              change for people throughout the entire training spectrum at an
-              advanced level, conducting and interpreting physical assessments,
-              and developing exercise prescriptions for those who are healthy or
-              have medically controlled diseases.
+              <em className="text-primary">Certified Exercise Physiologist</em>{" "}
+              through the American College of Sports Medicine (ACSM). Represents
+              the gold standard by creating change for people throughout the
+              entire training spectrum at an advanced level, conducting and
+              interpreting physical assessments, and developing exercise
+              prescriptions for those who are healthy or have medically
+              controlled diseases.
             </p>
           </span>
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.usaw className="h-24 w-auto flex-none object-contain" />
             <p className="flex-1 text-center sm:text-right">
-              Certified USA Weightlifting Level 2 for Olympic and Functional
-              Training. Expert level of teaching and coaching for all Olympic
-              lifts. Expert in teaching functional lifts and exercises for
-              physical health and wellness.
+              <em className="text-primary">
+                Certified USA Weightlifting Level 2 for Olympic and Functional
+                Training
+              </em>
+              . Expert level of teaching and coaching for all Olympic lifts.
+              Expert in teaching functional lifts and exercises for physical
+              health and wellness.
             </p>
           </span>
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.usatf className="order-1 h-24 w-auto flex-none object-contain" />
             <p className="order-1 flex-1 text-center sm:order-none sm:text-left">
-              Certified USA Track and Field Coach Level 1 for field and track
-              events. Expert in teaching and coaching sprint and speed training
+              <em className="text-primary">
+                Certified USA Track and Field Coach Level 1 for field and track
+                events
+              </em>
+              . Expert in teaching and coaching sprint and speed training
               techniques for short, middle, and long-distance running events and
               NFL combine training.
             </p>
@@ -73,17 +86,20 @@ export default function About() {
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.usapl className="h-24 w-auto flex-none object-contain" />
             <p className="flex-1 text-center sm:text-right">
-              Certified USA Powerlifting Senior Coach. Expert in teaching
-              fundamentals, progression, variations, and techniques for the
-              squat, bench press, and deadlift.
+              <em className="text-primary">
+                Certified USA Powerlifting Senior Coach
+              </em>
+              . Expert in teaching fundamentals, progression, variations, and
+              techniques for the squat, bench press, and deadlift.
             </p>
           </span>
           <span className="my-8 flex w-full flex-col items-center justify-between gap-8 sm:max-h-24 sm:flex-row">
             <Icons.issn className="order-1 h-24 w-auto flex-none object-contain" />
             <p className="order-1 flex-1 text-center sm:order-none sm:text-left">
-              Certified Sports Nutritionist with the working knowledge to take
-              your fitness level or sports career to the next level.
-              Personalized Nutrition guides are available to all clients.
+              <em className="text-primary">Certified Sports Nutritionist</em>{" "}
+              with the working knowledge to take your fitness level or sports
+              career to the next level. Personalized Nutrition guides are
+              available to all clients.
             </p>
           </span>
         </div>
--- app/wpcs/page.tsx
+++ app/wpcs/page.tsx
@@ -1,5 +1,6 @@
-import { Icons } from "@/components/icons"
 import Image from "next/image"
+
+import { Icons } from "@/components/icons"
 
 export default function WPCS() {
   return (
@@ -7,25 +8,33 @@ export default function WPCS() {
       <div className="mx-auto flex max-w-[980px] flex-col items-start gap-2">
         <div className="items-center justify-center self-center text-center">
           <h1 className="self-center text-4xl font-bold leading-tight">
-            THE WIL POWER CHAMPIONSHIP SERIES
+            THE <strong className="text-primary">WIL POWER CHAMPIONSHIP</strong>{" "}
+            SERIES
           </h1>
         </div>
         <Icons.weight className="w-72 self-center" />
         <p className="text-center">
-          The WPCS is a challenge for the Wil Power Championship belt. Every 3
-          to 4 months, the championship belt is put up for grabs. To win the
-          belt, you must win the month-long challenge. All athletes and clients
-          who actively train with Wil Power Sports Training are eligible to
-          compete. Those who choose to compete in the challenge are granted one
-          attempt per training session running through the whole month of the
-          challenge. It is the ultimate test of physical and mental toughness,
-          and those fortunate to win the championship belt exemplify those
-          attributes. The one who wins the belt keeps it till the next challenge
-          as they fight to retain their title. Should the current champion lose,
-          the belt will be handed off and awarded to the winner of the current
-          challenge, and so begins their reign as Wil Power Champion.
+          The <em className="text-primary">WPCS</em> is a challenge for the Wil
+          Power Championship belt. Every 3 to 4 months, the championship belt is
+          put up for grabs. To win the belt, you must win the month-long
+          challenge. All athletes and clients who actively train with Wil Power
+          Sports Training are eligible to compete. Those who choose to compete
+          in the challenge are granted one attempt per training session running
+          through the whole month of the challenge. It is the ultimate test of
+          physical and mental toughness, and those fortunate to win the
+          championship belt exemplify those attributes. The one who wins the
+          belt keeps it till the next challenge as they fight to retain their
+          title. Should the current champion lose, the belt will be handed off
+          and awarded to the winner of the current challenge, and so begins
+          their reign as Wil Power Champion.
         </p>
-        <Image className="mt-5 self-center" src="/wpcs.webp" alt="WPCS Belt" width={500} height={500} />
+        <Image
+          className="mt-5 self-center"
+          src="/wpcs.webp"
+          alt="WPCS Belt"
+          width={500}
+          height={500}
+        />
       </div>
     </section>
   )
