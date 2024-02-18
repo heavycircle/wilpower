c1d426b54fd96b8118717cdf2d84ece2573503dc
Cole <bleeper_genius0c@icloud.com>
2024-02-18 13:45:51 -0600

    fix: mobile formatting

--- config/site.ts
+++ config/site.ts
@@ -18,4 +18,22 @@ export const siteConfig = {
     phone: "tel:+16462103166",
     mail: "mailto:will52nt@hotmail.com",
   },
+  testimonials: [
+    {
+      quote: "I've been training with Wil for 5 months. My powerlifting total has increased over 100lbs. I've never felt stronger.",
+      name: "Cole Ellis",
+    },
+    {
+      quote: "Profound placeholder quote goes here that makes Wil look good.",
+      name: "Quote Author",
+    },
+    {
+      quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      name: "Lorem Ipsum",
+    },
+    {
+      quote: "This quote should scroll in from the right. Cool!",
+      name: "Wil Power",
+    }
+  ],
 }
